f098a32405803af359c3ef04954f104b8ae1dc89
hasna begum <h.b.muni16@gmail.com>
2023-05-25 14:56:00 +0100

    git push origin master

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,24 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    Bookmark.create(comment: bookmark_params[:comment], movie_id: bookmark_params[:movie_id], list_id: params[:list_id])
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
